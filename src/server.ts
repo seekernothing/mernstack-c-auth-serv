54597930efd45241d3ba17606d082b4ac1e69c14
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-03-09 23:59:07 +0530

    test lintstaged 3

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,5 @@
 function WelcomeMsg(name: string) {
     console.log(`Welcome ${name}`);
-
-    const user = {
-        name: "Abhishek",
-    };
-
-    const fname = user.name;
-
-    return name + fname;
 }
 
 WelcomeMsg("Abhishek");
