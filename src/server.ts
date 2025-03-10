d45ae4d1b01e2a8096d1706fae353258de037bab
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-03-11 03:53:09 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import { Config } from "./config";
 
 import app from "./config/app";
+import logger from "./config/logger";
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
-        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+        app.listen(PORT, () => {
+            logger.info("Server listening on port", { port: PORT });
+        });
     } catch (err) {
         console.log(err);
         process.exit(1);
