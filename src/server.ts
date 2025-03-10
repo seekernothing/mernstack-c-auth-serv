ea8dd97055584722829b692885b0415528c72693
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-03-11 00:47:25 +0530

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
 import { Config } from "./config";
 
-console.log(Config.PORT);
+import app from "./config/app";
+
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
+startServer();
