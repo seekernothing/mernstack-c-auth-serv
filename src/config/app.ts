ef8c9fb3f38f0317a9ef2f6022ee110d19fc5bdf
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-03-11 22:23:01 +0530

    add errorhandler

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,9 +1,31 @@
-import express from "express";
+// import express, { NextFunction } from "express";
+import logger from "./logger";
+import { HttpError } from "http-errors";
+import express, { Request, Response, NextFunction } from "express";
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Welcome to auth service");
+});
+
+// global error handler.(This is a global Middleware  )
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
